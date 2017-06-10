4ca3b15be5f34751b8b7016fefaa2c3ea761bcd4
LittleKey <LittleKeyRain@gmail.com>
2017-06-10 18:58:43 +0800

    update install script and update colorschemes compatibility tender

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ CURRENT_DIR=$(dirname "$0")
 
 if [ -d ${HOME}/.tmux ];
 then
-    mv ${HOME}/.tmux ${HOME}/.tmux.bak
+    mv -f ${HOME}/.tmux ${HOME}/.tmux.bak
 fi
 git submodule update --init --recursive
 mkdir -p ${HOME}/.tmux/plugins
-ln -sf ${CURRENT_DIR}/tpm ${HOME}/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ln -f ${CURRENT_DIR}/tmux.conf ${HOME}/.tmux.conf
 
