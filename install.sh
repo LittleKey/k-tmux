b2123fbcdbdea1cc412ef16e138ce3ce57cc6d5f
LittleKey <LittleKeyRain@gmail.com>
2026-02-12 09:58:10 +0000

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-CURRENT_DIR=$(dirname "$0")
+set -xe
 
-if [ -d ${HOME}/.tmux ];
-then
-  today=`date +%Y%m%d`
-  if $FOR_VIM; then
-    for i in ${HOME}/.tmux.conf ${HOME}/.tmux; do [ -e $i ] && [ ! -L $i ] && mv -f $i $i.$today; done
-    for i in ${HOME}/.tmux.conf ${HOME}/.tmux ; do [ -L $i ] && unlink $i ; done
-  fi
+CURRENT_DIR=$(pwd)
+
+if [ -d ${HOME}/.tmux ]; then
+	today=$(date +%Y%m%d)
+	if $FOR_VIM; then
+		for i in ${HOME}/.tmux.conf ${HOME}/.tmux; do [ -e $i ] && [ ! -L $i ] && mv -f $i $i.$today; done
+		for i in ${HOME}/.tmux.conf ${HOME}/.tmux; do [ -L $i ] && unlink $i; done
+	fi
 fi
 mkdir -p ${HOME}/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo 'exit 0' >> ~/.tmux/plugins/tpm/tpm
+echo 'exit 0' >>~/.tmux/plugins/tpm/tpm
 
 ln -f ${CURRENT_DIR}/tmux.conf ${HOME}/.tmux.conf
+mkdir -p ${HOME}/.config/tmux
+ln -f ${HOME}/.tmux.conf ${HOME}/.config/tmux/tmux.conf
 
 # install plugin and reload config
 tmux run-shell "${HOME}/.tmux/plugins/tpm/bindings/install_plugins"
